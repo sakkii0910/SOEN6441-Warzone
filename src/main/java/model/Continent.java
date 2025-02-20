63167e397b6012e14d75ad792c85d127ea59af8e
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2025-02-20 13:59:53 -0500

    - added ControlValue for each continent - added accessors and mutators (Continent)

--- src/main/java/model/Continent.java
+++ src/main/java/model/Continent.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Continent implements Serializable {
@@ -10,6 +11,46 @@ public class Continent implements Serializable {
 
     private String d_ContinentColor;
 
+    private int d_AwardedArmies;
+
     private Set<Country> d_ContinentCountries;
 
+    public int getContinentId() {
+        return d_ContinentId;
+    }
+
+    public void setContinentId(int p_ContinentId) {
+        this.d_ContinentId = p_ContinentId;
+    }
+
+    public String getContinentName() {
+        return d_ContinentName;
+    }
+
+    public void setContinentName(String p_ContinentName) {
+        this.d_ContinentName = p_ContinentName;
+    }
+
+    public String getContinentColor() {
+        return d_ContinentColor;
+    }
+
+    public void setContinentColor(String p_ContinentColor) {
+        this.d_ContinentColor = p_ContinentColor;
+    }
+
+    public int getAwardedArmies() {
+        return d_AwardedArmies;
+    }
+
+    public void setAwardedArmies(int p_AwardedArmies) {
+        this.d_AwardedArmies = p_AwardedArmies;
+    }
+
+    public Set<Country> getContinentCountries() {
+        if (d_ContinentCountries == null) {
+            d_ContinentCountries = new HashSet<>();
+        }
+        return d_ContinentCountries;
+    }
 }
