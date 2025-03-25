9885ca05b2edcd1ac29ebe0c55b1bd7c080c2933
poorav-panchal <pooravpanchal@gmail.com>
2025-03-25 17:23:54 -0400

    added checkDirectory method to LogEntryWriter.java

--- src/main/java/utils/logger/LogEntryWriter.java
+++ src/main/java/utils/logger/LogEntryWriter.java
@@ -22,12 +22,24 @@ public class LogEntryWriter implements Observer, Serializable {
     }
 
     /**
+     * Check if path is a folder
+     * @param path
+     */
+    private void checkDirectory(String path) {
+        File dir = new File (path);
+        if(!dir.exists() || !dir.isDirectory()) {
+            dir.mkdirs();
+        }
+    }
+
+    /**
      * Function to write log to file
      * @param p_s
      */
     public void writeLogFile(String p_s) {
         PrintWriter l_writer = null;
         try {
+            checkDirectory("logFiles");
             l_writer = new PrintWriter(new BufferedWriter(new FileWriter("logFiles/" + l_Filename + ".log", true)));
             l_writer.println(p_s);
         } catch (Exception e) {
@@ -42,6 +54,7 @@ public class LogEntryWriter implements Observer, Serializable {
      */
     public void clearLogs() {
         try {
+            checkDirectory("logFiles");
             File l_File = new File("logFiles/" + l_Filename + ".log");
             if(l_File.exists()) {
                 l_File.delete();
