d3391d6e2abf675d5633f33bdeb172876756a084
poorav-panchal <pooravpanchal@gmail.com>
2025-04-08 16:07:08 -0400

    added game progress class

---
+++ src/main/java/utils/GameProgress.java
@@ -0,0 +1,26 @@
+package utils;
+
+public class GameProgress {
+
+    /**
+     * Function to save game progress
+     * @return
+     */
+    public static boolean saveGameProgress() {
+        return true;
+    }
+
+    /**
+     * Function to load game
+     */
+    public static void loadGameProgress() {
+
+    }
+
+    /**
+     * Function to list all files
+     */
+    public static void showFiles() {
+
+    }
+}
